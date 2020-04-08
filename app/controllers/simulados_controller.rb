0eccd4b0df4408ece8f0a42dbd1ce45834cb660f
Rodrigo Barroso <rodrigogithub@gmail.com>
2020-04-08 18:17:46 -0300

    Página de show dos simulados adicionada

--- app/controllers/simulados_controller.rb
+++ app/controllers/simulados_controller.rb
@@ -1,5 +1,14 @@
 class SimuladosController < ApplicationController
+  before_action :set_simulado, only: [:show]
   def index
     @simulados = Simulado.all
   end
+
+  def show
+  end
+
+  private
+    def set_simulado
+      @simulado = Simulado.find(params[:id])
+    end
 end
