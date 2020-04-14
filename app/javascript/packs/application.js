df34e05b2c6defce0b5845a5fa88a6f063dd2504
Rodrigo Barroso <rodrigogithub@gmail.com>
2020-04-14 18:37:07 -0300

    Página de esqueceu sua senha e textos para emails atualizados

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("packs/main")
+require("jquery")
+require("packs/login")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
