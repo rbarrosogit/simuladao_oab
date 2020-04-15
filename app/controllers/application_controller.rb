f56619a9ab74e9957b9ef6afc8e118f1d9d76a3b
Rodrigo Barroso <rodrigogithub@gmail.com>
2020-04-15 16:03:33 -0300

    Nome do usuário adicionado a navbar logada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
 
   private
@@ -9,5 +10,12 @@ class ApplicationController < ActionController::Base
     else
       "application"
     end
+  end
+
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+
+  def first_name
+    self.name.blank? ? "" : self.name.split(" ")[0]
+  end
 end
