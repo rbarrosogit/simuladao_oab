1b2dd6f07a15f12088adf6bd1ab0ab5d2b670d6e
Rodrigo Barroso <rodrigogithub@gmail.com>
2020-04-27 18:58:13 -0300

    Porcentagem de acertos da prova e necessidade de login adicionados

--- app/controllers/simulados_controller.rb
+++ app/controllers/simulados_controller.rb
@@ -1,5 +1,7 @@
 class SimuladosController < ApplicationController
   before_action :set_simulado, only: [:show]
+  before_action :authenticate_user!, only: [:show]
+
   def index
     @simulados = Simulado.all
   end
@@ -8,12 +10,28 @@ class SimuladosController < ApplicationController
   end
 
   def finish
-    p "============================================="
-    p "passou aqui"
-    p "============================================="
-    byebug
+    @cont = 0
+    params.each do |key, value|
+      splited_key = key.split("-")
+      if (splited_key.length == 2 and splited_key.first == "question")
+        question_id = splited_key.last
+        question = Question.find(question_id)
+        if value == question.correct_answer
+          @cont += 1
+        end
+      end  
+    end
+    @percent = ((@cont / 5.0) * 100).to_i
+    if @percent >= 50
+      @percent_message = "Parabéns, você acertou mais da metade da prova."
+      if @percent == 100
+        @percent_message = "Incrível, você gabaritou a prova! Parabéns!"
+      end
+    else
+      @percent_message = "Que pena, parece que você precisa estudar mais."
+    end
   end
-
+  
 
   private
     def set_simulado
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  resources :simulados
+  resources :simulados do
+    resources :topics
+  end
   post '/simulados/finish', to: "simulados#finish", as: :finish_simulado
   resources :questions
   root to: 'home#index'
