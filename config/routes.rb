a370bac551e12d26e843d297d054bcfbf1651fab
Rodrigo Barroso <rodrigogithub@gmail.com>
2020-04-28 16:36:57 -0300

    Página inicial de perfil do usuário e troca de nome do usuários adicionados

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  resources :simulados
   root to: 'home#index'
+  resources :simulados
+  devise_for :users
+  get '/meuperfil', to: 'users#show', as: 'user'
+  get '/meuperfil/edit', to: 'users#edit', as: 'edit_name'
+  patch '/meuperfil', to: 'users#update'
+  put '/meuperfil', to: 'users#update'
 end
